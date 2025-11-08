4779c6790359def110e50ca4c22a4f339fbb7d6b
Geva-Eval Egbe <gevalinho@gmail.com>
2025-11-08 13:47:56 +0100

    Navigation using Tabs Navigation Patterns

---
+++ prodev-mobile-app-0x05/app/(home)/inbox.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+const inbox = () => (
+  <View>
+    <Text>Profile Screen</Text>
+  </View>
+);
+
+export default inbox;
---
+++ prodev-mobile-app-0x05/app/(home)/search.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+const search = () => (
+  <View>
+    <Text>Share Screen</Text>
+  </View>
+);
+
+export default search;
